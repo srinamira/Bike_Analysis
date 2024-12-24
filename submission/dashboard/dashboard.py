e602481a1bb742cbdc1216fe75a8300a0367d33b
sri namira <srinamira0@gmail.com>
2024-12-24 17:21:30 +0700

    modified:   submission/dashboard/dashboard.py

--- submission/dashboard/dashboard.py
+++ submission/dashboard/dashboard.py
@@ -1,8 +1,6 @@
 from datetime import datetime
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import plotly.express as px
 import plotly.graph_objects as go
 
 # Konfigurasi halaman
